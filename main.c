8d3a976c7aafa3d8d6676ae2f43be75fa87f2343
Petcu Cosmin <petcucosmin.contact@gmail.com>
2023-11-16 04:50:42 +0200

    Terminat S7

--- main.c
+++ main.c
@@ -2,13 +2,22 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <time.h>
+#include <string.h>
+#include <dirent.h>
 
-#define BUFFER_SIZE 285
- struct Images {
-    char imgName[50];
+#define NORMAL_FILE 0x00
+#define BMP_FILE    0x01
+#define LINK_FILE   0x02
+#define DIRECTORY   0x04
+
+#define BUFFER_SIZE 4098
+
+struct Files {
+    char fileName[50];
     int height;
     int width;
     int size;
+    int linkSize;
     int userId;
     int lastModifyDay;
     int lastModifyMonth;
@@ -17,99 +26,113 @@
     char userAccessRights[4];
     char groupAccessRights[4];
     char otherAccessRights[4];
-}image;
+}file;
 
-int readFileProprieties(char *fileName)
+int readFileProprieties(char *fileName, int fileType)
 {
     struct stat stats;
+    struct stat linkTarget;
     struct tm dt;
     int fisIn, rd;
     char buffer[BUFFER_SIZE];
-    const char *ext = strchr (fileName, '.');
-    if(!((ext!= NULL) && (!strcmp(ext+1,"bmp")))){
-        printf("You have read a different file, not a bmp");
-        return -1;
-    }
+
+
     if((fisIn = open(fileName,O_RDONLY)) == -1)
     {
         printf("Can't open file:%s",fileName);
         return -1;
     }
 
-    strcpy(image.imgName,fileName);
-
-    if((rd = read(fisIn, buffer, BUFFER_SIZE)) > 0)
+    if(fileType == BMP_FILE)
     {
-        memcpy(&image.width, buffer + 18, 4);
-        memcpy(&image.height, buffer + 22, 4);
+        if((rd = read(fisIn, buffer, BUFFER_SIZE)) > 0)
+        {
+            memcpy(&file.width, buffer + 18, 4);
+            memcpy(&file.height, buffer + 22, 4);
+        }
     }
+
     if (stat(fileName, &stats) == 0)
     {
+        if(fileType == LINK_FILE)
+        {
+            if(lstat(fileName, &linkTarget) == 0)
+            {
+                file.linkSize = stats.st_size;
+                file.size = linkTarget.st_size;
+            }
+        }
+        else
+        {
+            file.userId = stats.st_uid;
+        }
+
+        if(fileType == NORMAL_FILE || fileType == BMP_FILE)
+        {
+            file.size = stats.st_size;
+
+            dt = *(gmtime(&stats.st_mtime));
+            file.lastModifyDay = dt.tm_mday;
+            file.lastModifyMonth = dt.tm_mon;
+            file.lastModifyYear = dt.tm_year + 1900;
+
+            file.connectionsCounter = stats.st_nlink;
+        }
+
         if (stats.st_mode & S_IRUSR){
-            image.userAccessRights[0] = 'R';
+            file.userAccessRights[0] = 'R';
         } else {
-            image.userAccessRights[0] = '-';
+            file.userAccessRights[0] = '-';
         }
 
         if (stats.st_mode & S_IWUSR){
-            image.userAccessRights[1] = 'W';
+            file.userAccessRights[1] = 'W';
         } else {
-            image.userAccessRights[1] = '-';
+            file.userAccessRights[1] = '-';
         }
         if (stats.st_mode & S_IXUSR){
-            image.userAccessRights[2] = 'X';
+            file.userAccessRights[2] = 'X';
         } else {
-            image.userAccessRights[2] = '-';
+            file.userAccessRights[2] = '-';
         }
 
         if (stats.st_mode & S_IROTH){
-            image.otherAccessRights[0] = 'R';
+            file.otherAccessRights[0] = 'R';
         } else {
-            image.otherAccessRights[0] = '-';
+            file.otherAccessRights[0] = '-';
         }
 
         if (stats.st_mode & S_IWOTH){
-            image.otherAccessRights[1] = 'W';
+            file.otherAccessRights[1] = 'W';
         } else {
-            image.otherAccessRights[1] = '-';
+            file.otherAccessRights[1] = '-';
         }
         if (stats.st_mode & S_IXOTH){
-            image.otherAccessRights[2] = 'X';
+            file.otherAccessRights[2] = 'X';
         } else {
-            image.otherAccessRights[2] = '-';
+            file.otherAccessRights[2] = '-';
         }
 
         if (stats.st_mode & S_IRGRP){
-            image.groupAccessRights[0] = 'R';
+            file.groupAccessRights[0] = 'R';
         } else {
-            image.groupAccessRights[0] = '-';
+            file.groupAccessRights[0] = '-';
         }
 
         if (stats.st_mode & S_IWGRP){
-            image.groupAccessRights[1] = 'W';
+            file.groupAccessRights[1] = 'W';
         } else {
-            image.groupAccessRights[1] = '-';
+            file.groupAccessRights[1] = '-';
         }
         if (stats.st_mode & S_IXGRP){
-            image.groupAccessRights[2] = 'X';
+            file.groupAccessRights[2] = 'X';
         } else {
-            image.groupAccessRights[2] = '-';
+            file.groupAccessRights[2] = '-';
         }
 
-        image.groupAccessRights[3] = '\0';
-        image.userAccessRights[3] = '\0';
-        image.otherAccessRights[3] = '\0';
-
-        image.size = stats.st_size;
-
-        dt = *(gmtime(&stats.st_mtime));
-        image.lastModifyDay = dt.tm_mday;
-        image.lastModifyMonth = dt.tm_mon;
-        image.lastModifyYear = dt.tm_year + 1900;
-
-        image.connectionsCounter = stats.st_nlink;
-
-        image.userId = stats.st_uid;
+        file.groupAccessRights[3] = '\0';
+        file.userAccessRights[3] = '\0';
+        file.otherAccessRights[3] = '\0';
     }
     else
     {
@@ -121,41 +144,164 @@ int readFileProprieties(char *fileName)
     return 0;
 }
 
-int writeProprieties()
+int writeFileProprieties(int fileType)
 {
-    int fisOut;
+    int fisOut,chOut=0;
     char buffer[BUFFER_SIZE];
-    if((fisOut = open("C:\\Users\\cosmi\\CLionProjects\\so-proiect\\statistica.txt",O_CREAT | O_WRONLY | O_TRUNC)) == -1)
+
+    if((fisOut = open("statistica.txt",O_CREAT | O_WRONLY | O_APPEND)) == -1)
     {
         printf("Can't open file:statistica.txt");
         return -1;
     }
 
-    sprintf(buffer,"nume fisier:%s\n"
-                   "inaltime:%d\n"
-                   "lungime:%d\n"
-                   "dimensiune:%d\n"
-                   "identificatorul utilizatorului:%d\n"
-                   "timpul ultimei modificari:%d.%d.%d\n"
-                   "contorul de legaturi:%d\n"
-                   "drepturi de acces user:%s\n"
-                   "drepturi de acces grup:%s\n"
-                   "drepturi de acces altii:%s\n",
-                   image.imgName,
-                   image.height,
-                   image.width,
-                   image.size,
-                   image.userId,
-                   image.lastModifyDay,
-                   image.lastModifyMonth,
-                   image.lastModifyYear,
-                   image.connectionsCounter,
-                   image.userAccessRights,
-                   image.groupAccessRights,
-                   image.otherAccessRights);
-    buffer[strlen(buffer)] = '\0';
-    printf("%d\n",write(fisOut,buffer,BUFFER_SIZE));
+    if(fileType == BMP_FILE)
+    {
+        chOut = sprintf(buffer,"nume fisier:%s\n"
+                               "inaltime:%d\n"
+                               "lungime:%d\n"
+                               "dimensiune:%d\n"
+                               "identificatorul utilizatorului:%d\n"
+                               "timpul ultimei modificari:%d.%d.%d\n"
+                               "contorul de legaturi:%d\n"
+                               "drepturi de acces user:%s\n"
+                               "drepturi de acces grup:%s\n"
+                               "drepturi de acces altii:%s\n\n",
+                        file.fileName,
+                        file.height,
+                        file.width,
+                        file.size,
+                        file.userId,
+                        file.lastModifyDay,
+                        file.lastModifyMonth,
+                        file.lastModifyYear,
+                        file.connectionsCounter,
+                        file.userAccessRights,
+                        file.groupAccessRights,
+                        file.otherAccessRights);
+    }
+    else if(fileType == NORMAL_FILE)
+    {
+        chOut = sprintf(buffer,"nume fisier:%s\n"
+                               "dimensiune:%d\n"
+                               "identificatorul utilizatorului:%d\n"
+                               "timpul ultimei modificari:%d.%d.%d\n"
+                               "contorul de legaturi:%d\n"
+                               "drepturi de acces user:%s\n"
+                               "drepturi de acces grup:%s\n"
+                               "drepturi de acces altii:%s\n\n",
+                        file.fileName,
+                        file.size,
+                        file.userId,
+                        file.lastModifyDay,
+                        file.lastModifyMonth,
+                        file.lastModifyYear,
+                        file.connectionsCounter,
+                        file.userAccessRights,
+                        file.groupAccessRights,
+                        file.otherAccessRights);
+    }
+    else if(fileType == LINK_FILE)
+    {
+        chOut = sprintf(buffer,"nume legatura:%s\n"
+                               "dimensiune legatura:%d\n"
+                               "dimensiunea fisierului target:%d\n"
+                               "drepturi de acces user legatura:%s\n"
+                               "drepturi de acces grup legatura:%s\n"
+                               "drepturi de acces altii legatura:%s\n\n",
+                        file.fileName,
+                        file.linkSize,
+                        file.size,
+                        file.userAccessRights,
+                        file.groupAccessRights,
+                        file.otherAccessRights);
+    }
+    else if(fileType == DIRECTORY)
+    {
+        chOut = sprintf(buffer,"nume director:%s\n"
+                               "identificatorul utilizatorului:%d\n"
+                               "drepturi de acces user:%s\n"
+                               "drepturi de acces grup:%s\n"
+                               "drepturi de acces altii:%s\n\n",
+                        file.fileName,
+                        file.userId,
+                        file.userAccessRights,
+                        file.groupAccessRights,
+                        file.otherAccessRights);
+    }
+
+    if(chOut != 0)
+    {
+        write(fisOut,buffer,chOut);
+    }
+    else
+    {
+        printf("No proprieties to get.\n");
+    }
+
+
     close(fisOut);
+    return 0;
+}
+
+int getFileProprieties(char *folderName)
+{
+    struct dirent *files;
+    struct stat stats;
+    DIR *folder;
+    char filePath[BUFFER_SIZE];
+    const char *ext;
+
+    if((folder = opendir(folderName)) == NULL)
+    {
+        printf("Can't open folder\n");
+        return -1;
+    }
+
+    while((files = readdir(folder)) != NULL)
+    {
+        sprintf(filePath,"%s/%s",folderName,files->d_name);
+        if((stat(filePath, &stats) == 0))
+        {
+            if(S_ISREG(stats.st_mode))
+            {
+                strcpy(file.fileName,files->d_name);
+                ext = strchr (filePath, '.');
+                if(((ext!= NULL) && (!strcmp(ext+1,"bmp"))))
+                {
+
+                    readFileProprieties(filePath,BMP_FILE);
+                    writeFileProprieties(BMP_FILE);
+                }
+                else
+                {
+                    readFileProprieties(filePath,NORMAL_FILE);
+                    writeFileProprieties(NORMAL_FILE);
+                }
+            }
+            else if(S_ISLNK(stats.st_mode))
+            {
+                strcpy(file.fileName,files->d_name);
+                readFileProprieties(filePath,LINK_FILE);
+                writeFileProprieties(LINK_FILE);
+            }
+            else if(S_ISDIR(stats.st_mode))
+            {
+                if(strcmp(files->d_name,".") != 0 && strcmp(files->d_name,"..") != 0)
+                {
+                    strcpy(file.fileName,files->d_name);
+                    readFileProprieties(filePath,DIRECTORY);
+                    writeFileProprieties(DIRECTORY);
+                }
+            }
+            else
+            {
+                printf("No proprieties will be shown.\n");
+            }
+        }
+    }
+
+    closedir(folder);
     return 0;
 }
 
@@ -165,7 +311,6 @@ int main(int argv, char **argc)
         printf("Error number of arguments");
         return -1;
     }
-    readFileProprieties(argc[1]);
-    writeProprieties();
+    getFileProprieties(argc[1]);
     return 0;
 }
